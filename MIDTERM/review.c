f65080c6f1464a64f194440573ac85f317cbff42
JD Biñegas <john.binegas@lpunetwork.edu.ph>
2023-05-31 18:34:49 +0800

    final

--- MIDTERM/review.c
+++ MIDTERM/review.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-typedef struct Student_Accounts {
+typedef struct{
     char *student_name;
     char *student_number;
     char *Year_Level;
 } Student_Accounts;
 
 Student_Accounts students []= {
-    {"Jandell Ang","2022-1-0155","1st Year"},
-    {"Bermard Velez","2022-1-0154","1st Year"}
+    {"John Doe","2022-1-0155","1st Year"},
+    {"Jane Doe","2022-1-0154","1st Year"}
 };
 
 
@@ -21,20 +21,36 @@ void Print_Student_Accounts()
  
 void Print_Salary()
 {
-    float salary;
-    float *psalary;
+   float salary;
+    float* ptr_salary;
 
     salary = 5000;
-    psalary = &salary;
+    ptr_salary = &salary;
 
     printf("Enter Salary: ");
-    scanf("%f\n",psalary);
+    scanf("%f", ptr_salary);
 
-    printf("Salary: %f",*psalary);
+    printf("Salary: %.2f", *ptr_salary);
 }
+
+void print_array() {
+    float numbers[] = {1.5, 2.0, 3.5, 4.0, 5.5, 6.0, 7.5, 8.0, 9.5, 10.0};
+    float product = 1.0;
+    float *ptr_product;
+    ptr_product = &product;
+
+    for (int i = 0; i < 10; i++) 
+        product *= numbers[i];
+
+    printf("Product: %.2f", *ptr_product);
+}
+
 int main ()
 {
+    printf("\n\n");
     Print_Student_Accounts();
     printf("\n\n\n");
     Print_Salary();
+    printf("\n\n\n");
+    print_array();
 }
